0528a0c29f920045deedc2a23db7bfec81e67a2c
Marcin Filipek <filipekmarcin180@gmail.com>
2022-09-06 21:23:10 +0200

    Added products query

--- pages/products/index.tsx
+++ pages/products/index.tsx
@@ -1,5 +1,24 @@
-function Products() {
-  return <p>Products list</p>;
+import { prisma } from "../../server/db/client";
+import { Product } from "./types";
+
+type Props = {
+  products: Product[];
+};
+
+function Products({ products }: Props) {
+  return (
+    <ul>
+      {products.map((product) => (
+        <li key={product.id}>{product.name}</li>
+      ))}
+    </ul>
+  );
+}
+
+export async function getServerSideProps() {
+  const products = await prisma.product.findMany();
+
+  return { props: { products } };
 }
 
 export default Products;
