ec5d1c286a0943ce5a4a667a91090ef750c570f4
MarcinFilipek <filipekmarcin180@gmail.com>
2022-09-11 21:52:32 +0200

    Product page (#4)
    * Added layout with nav, product page

--- pages/products/[id].tsx
+++ pages/products/[id].tsx
@@ -3,33 +3,34 @@ import {
   GetStaticProps,
   InferGetStaticPropsType,
   NextPage,
-} from "next";
-import { prisma } from "../../server/db/client";
-import { ParsedUrlQuery } from "querystring";
+} from 'next'
+import { prisma } from '../../server/db/client'
+import { Box, Typography } from '@mui/material'
 
 type Product = {
-  name: string;
-  id: number;
-  description: string;
-};
+  name: string
+  id: number
+  description: string
+  price: number
+}
 
 export const getStaticPaths: GetStaticPaths = async () => {
-  const products = await prisma.product.findMany();
+  const products = await prisma.product.findMany()
 
   const paths = products.map((product) => ({
     params: { id: product.id.toString() },
-  }));
+  }))
 
-  return { paths, fallback: false };
-};
+  return { paths, fallback: false }
+}
 
 type ContextParams = {
-  id: string;
-};
+  id: string
+}
 
 type PageProps = {
-  product: Product | null;
-};
+  product: Product | null
+}
 
 export const getStaticProps: GetStaticProps<PageProps, ContextParams> = async ({
   params,
@@ -39,19 +40,25 @@ export const getStaticProps: GetStaticProps<PageProps, ContextParams> = async ({
       where: {
         id: +params.id,
       },
-    });
-    return { props: { product } };
+    })
+    return { props: { product } }
   }
-  return { props: { product: null } };
-};
+  return { props: { product: null } }
+}
 
 const ProductPage: NextPage<InferGetStaticPropsType<typeof getStaticProps>> = ({
   product,
 }) => {
   if (product) {
-    return <p>{product.description}</p>;
+    return (
+      <Box>
+        <Typography variant="h3">{product.name}</Typography>
+        <Typography variant="body1">{product.description}</Typography>
+        <Typography variant="body2">{product.price}</Typography>
+      </Box>
+    )
   }
-  return <p>Error</p>;
-};
+  return <p>Error</p>
+}
 
-export default ProductPage;
+export default ProductPage
--- pages/products/index.tsx
+++ pages/products/index.tsx
@@ -1,22 +1,51 @@
-import { InferGetStaticPropsType } from "next";
-import { prisma } from "../../server/db/client";
+import { InferGetStaticPropsType } from 'next'
+import { prisma } from '../../server/db/client'
+import { Grid, Paper, Typography, Box } from '@mui/material'
+import Link from 'next/link'
 
 export async function getStaticProps() {
-  const products = await prisma.product.findMany();
+  const products = await prisma.product.findMany()
 
-  return { props: { products } };
+  return { props: { products } }
 }
 
 function Products({
   products,
 }: InferGetStaticPropsType<typeof getStaticProps>) {
   return (
-    <ul>
-      {products.map((product) => (
-        <li key={product.id}>{product.name}</li>
-      ))}
-    </ul>
-  );
+    <>
+      <Typography variant="h3" sx={{ pb: 3 }}>
+        Products
+      </Typography>
+      <Grid container>
+        {products.map((product) => (
+          <Grid item key={product.id} lg={3}>
+            <Link href={`/products/${product.id}`}>
+              <Paper sx={{ p: 2, cursor: 'pointer' }}>
+                <Typography variant="h6" sx={{ pb: 2 }}>
+                  {product.name}
+                </Typography>
+                <Box
+                  sx={{
+                    width: '100%',
+                    display: 'flex',
+                    justifyContent: 'end',
+                  }}
+                >
+                  <Typography
+                    variant="body1"
+                    sx={{ color: 'gray', fontWeight: 700 }}
+                  >
+                    {product.price}
+                  </Typography>
+                </Box>
+              </Paper>
+            </Link>
+          </Grid>
+        ))}
+      </Grid>
+    </>
+  )
 }
 
-export default Products;
+export default Products
